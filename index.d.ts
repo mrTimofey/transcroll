0ca8e55d5d2167ea5bdffa8eceaedf631508ee2d
Timofey Tiunov <timoxa17@gmail.com>
2018-11-15 16:37:41 +0300

    typings for built-in easing functions

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,16 @@
 declare module 'transcroll' {
+	type easeFn = (arg: number) => number;
+	type easeName = 'linear' | 'easeInQuad' | 'easeOutQuad' | 'easeInOutQuad' | 'easeInCubic' | 'easeOutCubic' | 'easeInOutCubic' |
+		'easeInQuart' | 'easeOutQuart' | 'easeInOutQuart' | 'easeInQuint' | 'easeOutQuint' | 'easeInOutQuint';
 	let transcroll: (target: Window | Element | number | string, args?: {
 		el?: Window | Element,
 		offset?: number,
 		axis?: 'x' | 'y',
 		duration?: number,
-		easing?: string | ((x: number) => number),
+		easing?: easeName | easeFn,
 		jump?: false | number,
 		interrupt?: boolean
 	}) => Promise<{ interrupted: boolean, jumped: boolean }>;
 	export default transcroll;
+	export let easings: { [name in easeName]: easeFn };
 }
